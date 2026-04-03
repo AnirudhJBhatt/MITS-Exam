b6f944c0d9a0231a1cb17f6a654585a9d86033a8
Anirudh J Bhatt <23mca08@mgits.ac.in>
2026-04-03 11:07:21 +0530

    MITS-Exam

--- Dept_Admin/dashboard.php
+++ Dept_Admin/dashboard.php
@@ -7,7 +7,7 @@
 	require_once "../Connection/connection.php";
 ?>
 <html lang="en">
-<!-- Hello -->
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
