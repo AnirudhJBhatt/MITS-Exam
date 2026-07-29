bdae76136975e8e58c416a60a585c4880b7c5250
AnirudhJBhatt <23mca08@mgits.ac.in>
2026-07-29 12:29:34 +0530

    MITS-Exam

--- Student/attempt-exam-new.php
+++ Student/attempt-exam-new.php
@@ -6,7 +6,7 @@
     }
 	require_once __DIR__ . "/../Connection/connection.php";
 	$Stud_ID = $_SESSION['LoginStudent'];
-	$exam_id = isset($_GET['exam_id']) ? (int)$_GET['exam_id'] : 4;
+	$exam_id = (int)$_GET['exam_id'];
 
 	if ($exam_id <= 0) {
 		die("Invalid Exam ID");
